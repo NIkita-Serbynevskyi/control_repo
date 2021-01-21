26777d88b033e027f84ba8d31bb5789ba68c45f3
NIkita-Serbynevskyi <77770309+NIkita-Serbynevskyi@users.noreply.github.com>
2021-01-21 10:52:30 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,6 +12,6 @@ class profile::ssh_server {
           ensure => present,
           user   => 'root',
           type   => 'ssh-rsa',
-          key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCgmvwqtIO0sn6Vg+M/OPl0Kx5rZp3BYy2hbeLeUXBOfiqx6o+EhIg3s+m5rsuOntZ9xJ9UoBKx88ntEckipQ6Yt5UgzCtLaPiBSr8uNX7nQQIdzV+z8K65pTdledcGFg916Ydk7xrQqAwFB3aqEv9lgFJJEfbbDqn3/sl8dWSHcbIOOWYlEH2kucoUU1bnrp7Yxzpom5g1S9yMRoxCamQJkM/MMAIRBbD22VncDWvzqfsJWC78YdHhsHa3hFQ3IdIaAuHSfRnca7N5gjVeKe3NR80C6/rLIaEL8jynAydv9iCDKhIBRjUonmAFrb8m7mlX8R1kwG2K9nZRoajZzhBN root@master.puppet.vm', 
+          key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCgmvwqtIO0sn6Vg+M/OPl0Kx5rZp3BYy2hbeLeUXBOfiqx6o+EhIg3s+m5rsuOntZ9xJ9UoBKx88ntEckipQ6Yt5UgzCtLaPiBSr8uNX7nQQIdzV+z8K65pTdledcGFg916Ydk7xrQqAwFB3aqEv9lgFJJEfbbDqn3/sl8dWSHcbIOOWYlEH2kucoUU1bnrp7Yxzpom5g1S9yMRoxCamQJkM/MMAIRBbD22VncDWvzqfsJWC78YdHhsHa3hFQ3IdIaAuHSfRnca7N5gjVeKe3NR80C6/rLIaEL8jynAydv9iCDKhIBRjUonmAFrb8m7mlX8R1kwG2K9nZRoajZzhBN root@master.puppet.vm', 
         }
 }
